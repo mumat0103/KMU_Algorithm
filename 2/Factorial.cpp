a59481746fb3eb6d9236a422eae69975c3eedede
mumat0103 <wsx2138@naver.com>
2022-09-27 19:41:57 +0900

    fix:팩토리얼

--- 2/Factorial.cpp
+++ 2/Factorial.cpp
@@ -22,18 +22,6 @@ int main()
     return 0;
 }
 
-long long facto(int n)
-{
-    for (int i = 1; i <= n; i++)
-    {
-        ans *= i;
-        ans %= 10000000;
-        while (ans % 10 == 0)
-            ans /= 10;
-    }
-    return ans % 1000;
-}
-
 int facto_recur(int n)
 {
     if (n <= 1)
@@ -41,13 +29,10 @@ int facto_recur(int n)
     else
     {
         ans = n * facto_recur(n - 1);
-        // printf("before ans : %d\n", ans);
         while (ans % 10 == 0)
         {
             ans /= 10;
-            // printf("after ans : %d, %d\n", ans, n);
         }
     }
     return ans % 10000;
-    // return n * facto_recur(n - 1);
 }
